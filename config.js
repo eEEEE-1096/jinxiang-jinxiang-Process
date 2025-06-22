25f4fe24de982c4991349a7011b4babffc46537e
jinxianggongyi <3780027024@qq.com>
2025-06-22 12:41:33 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+// 城建进度数据
+const constructionData = {
+    subway: {
+        images: [
+            // 地下轨道交通图片路径
+        ],
+        announcement: '正在建设中，敬请期待。'
+    },
+    intercity: {
+        images: [
+            // 城际轨道交通图片路径
+        ],
+        announcement: '正在建设中，敬请期待。。'
+    },
+    city: {
+        images: [
+            // 城市建设图片路径
+        ],
+        announcement: '正在建设中，敬请期待。'
+    }
+};
+
+// 处罚公告数据
+const punishmentData = [
+    {
+        title: '暂无',
+        player: '',
+        violation: '',
+        punishment: '',
+        date: '',
+        images: []
+    }
+];
